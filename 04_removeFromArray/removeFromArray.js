f66848384910fc281d6400cea738deda72346ef5
Marcin <marcin.obrebowski@outlook.com>
2022-12-27 23:12:44 +0100

    Do all the tasks and pass all the tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...args) {
+  let newArr = arr;
+  for (let i = 0; i < args.length; i++) {
+    newArr = newArr.filter(function (el) {
+      return el !== args[i];
+    });
+  }
+  return newArr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let finalSum = 0;
+  if (typeof a === "number" && typeof b === "number" && a >= 0 && b >= 0) {
+    if (a < b) {
+      for (let i = a; i <= b; i++) {
+        finalSum += i;
+      }
+    } else {
+      for (let i = b; i <= a; i++) {
+        finalSum += i;
+      }
+    }
+  } else {
+    return "ERROR";
+  }
+  return finalSum;
 };
 
 // Do not edit below this line
