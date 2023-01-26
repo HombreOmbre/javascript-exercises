2313118332ec2734c4e18ae2001e647fe95ecc80
HombreOmbre <marcin.obrebowski@outlook.com>
2023-01-26 23:05:16 +0100

    Pass all exams

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  let counter = [1];
+  if (n < 0) {
+    return "OOPS";
+  } else {
+    if (counter.length === 1) {
+      counter.push(1);
+      for (let i = 1; i < n; i++) {
+        counter.push(counter[i] + counter[i - 1]);
+      }
+    } else {
+      for (let i = 1; i < n; i++) {
+        counter.push(counter[i] + counter[i - 1]);
+      }
+    }
+  }
+  return counter[n - 1];
 };
 
 // Do not edit below this line
