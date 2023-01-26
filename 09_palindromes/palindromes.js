257ef467c7f6160340a16da0f290d92e0b6c65d6
HombreOmbre <marcin.obrebowski@outlook.com>
2023-01-26 20:47:20 +0100

    Passed all palindromes test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let counter = 0;
+  const validatedStr = str
+    .replace(/[!-.,]/g, "")
+    .toLowerCase()
+    .split(" ")
+    .join("");
+  const backwardStr = validatedStr.split("").reverse().join("");
+  for (let i = 0; i < validatedStr.length; i++) {
+    if (validatedStr[i] === backwardStr[i]) {
+      counter++;
+    }
+  }
+  return counter === validatedStr.length;
 };
 
 // Do not edit below this line
