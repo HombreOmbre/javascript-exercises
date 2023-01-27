78180d722b235477a27ec9136af4accdb8e3d21d
HombreOmbre <marcin.obrebowski@outlook.com>
2023-01-27 14:14:41 +0100

    Pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = (arr) => {
+  return arr.reduce((a, b) => {
+    const aAge = getAge(a.yearOfBirth, a.yearOfDeath);
+    const bAge = getAge(b.yearOfBirth, b.yearOfDeath);
+    return aAge < bAge ? b : a;
+  });
+};
 
+const getAge = (birth, death) => {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
 };
 
 // Do not edit below this line
